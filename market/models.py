1f231a2d3c2964ae02338f970afc504869b507b9
Giga Samkharadze <gigasamkharadzee@gmail.com>
2024-04-08 19:41:19 +0400

    translation for model fields

--- market/models.py
+++ market/models.py
@@ -1,21 +1,22 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 
 # Create your models here.
 class Book(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Name')
-    page_count = models.IntegerField(verbose_name='Page count')
-    category = models.CharField(max_length=100, verbose_name='Category')
-    author_name = models.CharField(max_length=100, verbose_name='Author name')
+    name = models.CharField(max_length=100, verbose_name=_('name'))
+    page_count = models.IntegerField(verbose_name=_('page count'))
+    category = models.CharField(max_length=100, verbose_name=_('category'))
+    author_name = models.CharField(max_length=100, verbose_name=_('author name'))
     price = models.IntegerField(verbose_name='Price')
-    image = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name='Image')
+    image = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name=_('image'))
 
     def __str__(self):
         return f'{self.name} written by {self.author_name}'
 
     class Meta:
-        verbose_name = 'Book'
-        verbose_name_plural = 'Books'
+        verbose_name = _('book')
+        verbose_name_plural = _('books')
         ordering = ['name', 'price', 'page_count', 'author_name']
         indexes = [
             models.Index(fields=['name'], name='name_idx'),
--- market/views.py
+++ market/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.core.serializers import serialize
+
 from market.models import Book
 
 
