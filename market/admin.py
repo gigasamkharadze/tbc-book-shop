5df627157af287e90acea0ec5836b634a17de60b
Giga Samkharadze <gigasamkharadzee@gmail.com>
2024-04-14 16:02:49 +0400

    author and category models. cover type choices

--- market/admin.py
+++ market/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
-from market.models import Book
+from market.models import Book, Author, Category
 
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
-    list_display = ['name', 'page_count', 'category', 'author_name', 'price', 'image']
-    search_fields = ['name', 'author_name']
+    list_display = ['name', 'page_count', 'category', 'cover', 'author', 'price', 'image']
+    search_fields = ['name', 'author__first_name']
+
+    def author(self, obj):
+        return f'{obj.author.first_name} {obj.author.last_name}'
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'date_of_birth', 'country']
+    search_fields = ['first_name', 'last_name']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
 
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Category, CategoryAdmin)
--- market/models.py
+++ market/models.py
@@ -4,26 +4,71 @@ from django.utils.translation import gettext as _
 
 # Create your models here.
 class Book(models.Model):
+    HARD_COVER = 'HC'
+    SOFT_COVER = 'SC'
+    SPECIAL_COVER = 'SPC'
+    COVER_CHOICES = [
+        (HARD_COVER, _('Hard cover')),
+        (SOFT_COVER, _('Soft cover')),
+        (SPECIAL_COVER, _('Special cover'))
+    ]
+
     name = models.CharField(max_length=100, verbose_name=_('name'))
     page_count = models.IntegerField(verbose_name=_('page count'))
-    category = models.CharField(max_length=100, verbose_name=_('category'))
-    author_name = models.CharField(max_length=100, verbose_name=_('author name'))
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='books', verbose_name=_('category'))
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, related_name='books', verbose_name=_('author'))
     price = models.IntegerField(verbose_name='Price')
+    cover = models.CharField(max_length=3, choices=COVER_CHOICES, default=SOFT_COVER, verbose_name=_('cover type'))
     image = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name=_('image'))
 
     def __str__(self):
-        return f'{self.name} written by {self.author_name}'
+        return f'{self.name} written by {self.author.first_name}'
 
     class Meta:
         verbose_name = _('book')
         verbose_name_plural = _('books')
-        ordering = ['name', 'price', 'page_count', 'author_name']
+        ordering = ['name', 'price', 'page_count', 'category', 'author']
         indexes = [
-            models.Index(fields=['name'], name='name_idx'),
             models.Index(fields=['price'], name='price_idx'),
             models.Index(fields=['page_count'], name='page_count_idx'),
-            models.Index(fields=['author_name'], name='author_name_idx')
+        ]
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name=_('first name'))
+    last_name = models.CharField(max_length=100, verbose_name=_('last name'))
+    date_of_birth = models.DateField(null=True, verbose_name=_('date of birth'))
+    country = models.CharField(max_length=100, verbose_name=_('country'))
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} from {self.country}. birth date: {self.date_of_birth}'
+
+    class Meta:
+        verbose_name = _('author')
+        verbose_name_plural = _('authors')
+        ordering = ['first_name', 'last_name', 'date_of_birth']
+        indexes = [
+            models.Index(fields=['first_name'], name='first_name_idx'),
+            models.Index(fields=['last_name'], name='last_name_idx'),
         ]
         constraints = [
-            models.UniqueConstraint(fields=['name', 'author_name'], name='unique_book')
+            models.UniqueConstraint(fields=['first_name', 'last_name'], name='unique_author'),
+        ]
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name=_('name'))
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = _('category')
+        verbose_name_plural = _('categories')
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name'], name='name_idx'),
+        ]
+        constraints = [
+            models.UniqueConstraint(fields=['name'], name='unique_category'),
         ]
